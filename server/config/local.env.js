4c4142f229fb825f03cc39813b007fc653b5b5e8
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2015-04-20 11:32:11 -0300

    changed expres.js

---
+++ server/config/local.env.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  DOMAIN: 'http://localhost:5000',
+  SESSION_SECRET: "electionanalysis-secret",
+
+  FACEBOOK_ID: 'app-id',
+  FACEBOOK_SECRET: 'secret',
+
+  TWITTER_ID: 'app-id',
+  TWITTER_SECRET: 'secret',
+
+  GOOGLE_ID: 'app-id',
+  GOOGLE_SECRET: 'secret',
+
+  DEBUG: ''
+};
